d06f7330dd93a5a612eb5ef6878b1141ffdc6f79
Adam A.G. Shamblin <adam.shamblin@gmail.com>
2017-07-24 19:33:05 -0600

    Stub for future tests.

---
+++ zeroecks/tests/test_user.py
@@ -0,0 +1,11 @@
+import unittest
+from unittest.mock import MagicMock
+
+
+class TestUser(unittest.TestCase):
+
+    def test_hash_password(self):
+        self.assertTrue(True)
+
+    def test_foo(self):
+        self.assertTrue(True)
